2dbc90e29088bf91f956f22eb2eb9d66863436cf
bega0306 <bega0306@gmail.com>
2019-12-21 00:19:49 +0900

    #5 Deploying to Github Pages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Movie App 2019
 
 본 프로젝트는 리액트를 배우기 위해 만들어졌습니다.
+
+url: https://bega0306.github.io/movie_app_2019/
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropType from 'prop-types';
+import './Movie.css';
 
-function Movie({id, year, title, summary, poster}){
-    return <h1>{title}</h1>;
+function Movie({id, year, title, summary, poster, genres}){
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__data">
+                <h1 className="movie__title">{title}</h1>
+                <h4 className="movie__year">{year}</h4>
+                <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.prototype = {
@@ -10,7 +21,8 @@ Movie.prototype = {
     year: PropType.number.isRequired,
     title: PropType.string.isRequired,
     summary: PropType.string.isRequired,
-    poster: PropType.string.isRequired
+    poster: PropType.string.isRequired,
+    genres: PropType.arrayOf(PropType.string).isRequired
 }
 
 export default Movie;
